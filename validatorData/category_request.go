acafef65838864501555c3c17b0ad4d50c8ee687
junkai1992 <xujunkaipy@163.com>
2020-09-12 16:55:18 +0800

    update .gitignore

--- validatorData/category_request.go
+++
@@ -1,9 +0,0 @@
-package validatorData
-
-type CreateCategoryRequest struct {
-	Name string `json:"name" binding:"required"`
-}
-
-type CreateVisitRequest struct {
-	Visitcount int `json:"visitcount" binding:"required"`
-}
--- validatorData/post_request.go
+++
@@ -1,9 +0,0 @@
-package validatorData
-
-type CreatePostRequetst struct {
-	CategoryId uint   `json:"category_id" binding:"required"`
-	Title      string `json:"title" binding:"required,max=40"`
-	Preface    string `json:"preface" bind:"required"`
-	HeadImg    string `json:"head_img"`
-	Content    string `json:"content" binding:"required"`
-}
